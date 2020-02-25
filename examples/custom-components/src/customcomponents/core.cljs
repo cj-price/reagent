dfc219f9ca45599b61118b49d4f7efdbd911bd6b
C.J. Price <42616742+cj-price@users.noreply.github.com>
2020-02-25 12:30:49 -0500

    Added more to example

--- examples/custom-components/src/customcomponents/core.cljs
+++ examples/custom-components/src/customcomponents/core.cljs
@@ -2,15 +2,13 @@
   (:require [reagent.core :as r]))
 
 
-(defn Note [props child]
-  [:div props
-   "NOTICE! "
-   child])
+(defn Note [props & children]
+  (into [:div props "NOTICE! "] children))
 
 (def ClassNote
   (r/reactify-component
-   (fn [props child]
-     [:div "ADAPTED NOTICE! " child])))
+   (fn [props & children]
+     (into [:div "ADAPTED NOTICE! "] children))))
 
 (r/set-custom-components! {:Note #'Note
                            :ClassNote ClassNote})
@@ -19,9 +17,17 @@
   [:<>
    [:Note#note1 "Here is a simple note"]
    [:Note#note2>span.color-blue "Here is a blue note"]
-   [:div#note3-wrapper.bg-blue>Note#note3 "Here is a blue background note"]
-   [:Note#nested-note>Note#child-note "Here is a nested note"]
-   [:ClassNote#class-note.color-blue>Note "Here is an adapted react class"]])
+   [:div#note3-wrapper.bg-blue>Note#note3
+    {:style {:color "red"}}
+    "Here is a blue background note"]
+   [:Note#nested-note>Note#child-note
+    "Here is a nested note"
+    [:div "with multiple children"]]
+   [:ClassNote#class-note.color-blue>Note
+    {:style {:background-color "red"}}
+    "Here is an adapted react class"
+    [:div "with multiple children"]
+    [:div "and props"]]])
 
 (defn ^:export run []
   (r/render [custom-components-example]
